40da97c5557ffadf9669fb324bc86588e97bd616
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2025-04-02 17:38:43 +0100

    make it one big graph now

--- public/graph.js
+++ public/graph.js
@@ -2,63 +2,63 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     let { economy, skyblock } = await fetch(`${document.location.origin}/data`).then(res => res.json());
 
-    var eco = new Chart('ecochart', {
-        type: 'line',
-        data: {
-            labels: economy.x,
-            datasets: [{
-                label: 'Economy Players Online',
-                data: economy.y,
-                backgroundColor: 'rgba(0, 255, 0, 0.4)',
-                borderColor: 'rgba(0, 255, 0, 1)',
-            }]
-        },
-        options: {
-            animation: false,
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    })
 
-    var net = new Chart('netchart', {
+    var graph = new Chart('graph', {
         type: 'line',
         data: {
             labels: skyblock.x,
             datasets: [{
                 label: 'Skyblock Players Online',
                 data: skyblock.y,
-                backgroundColor: 'rgba(255, 230, 0, 0.4)',
+                // backgroundColor: 'rgba(255, 230, 0, 0.4)',
                 borderColor: 'rgba(255, 255, 0, 1)',
+                fill: false,
+            }, {
+                label: 'Economy Players Online',
+                data: economy.y,
+                // backgroundColor: 'rgba(0, 255, 0, 0.4)',
+                borderColor: 'rgba(0, 255, 0, 1)',
+                fill: false,
             }]
         },
         options: {
             animation: false,
+            responsive: true,
+            maintainAspectRatio: false,
+            elements: {
+                point: {
+                    radius: 0,          // Set to 0 to remove the points/bubbles
+                    hitRadius: 10,       // Adjust this as needed for hover detection
+                    hoverRadius: 0,     // Remove the bubble on hover, too
+                    hoverBorderWidth: 0  // Remove border during hover
+                }
+            },
+            tooltips: {
+                mode: 'index',        // 'index' mode is crucial
+                intersect: false,      // Ensure tooltip is triggered even if the point isn't directly hovered
+            },
+            legend: {
+                align: 'start' // 'start' aligns to the left
+            },
             scales: {
                 yAxes: [{
                     ticks: {
                         beginAtZero: true
                     }
-                }]
+                }],
+                xAxes: [{
+                    time: {
+                        stepSize: 1, // Adjust this value
+                    },
+                }],
             }
         }
     })
 
-    const net_canvas = document.querySelector('#netchart')
-    net_canvas.addEventListener('click', (e) => {
+    const graph_canvas = document.querySelector('#graph')
+    graph_canvas.addEventListener('click', (e) => {
         var a = document.createElement('a');
         a.href = net.toBase64Image();
-        a.target = '_blank';
-        a.click();
-    })
-    const eco_canvas = document.querySelector('#ecochart')
-    eco_canvas.addEventListener('click', () => {
-        var a = document.createElement('a');
-        a.href = eco.toBase64Image();
         a.target = '_blank';
         a.click();
     })
@@ -68,16 +68,11 @@ document.addEventListener('DOMContentLoaded', async function () {
 
         let { economy, skyblock } = await fetch(`${document.location.origin}/data`).then(res => res.json());
 
-        eco.data.labels = economy.x
-        net.data.labels = skyblock.x
+        graph.data.labels = skyblock.x
 
-        eco.data.datasets[0].data = economy.y;
-        net.data.datasets[0].data = skyblock.y;
+        graph.data.datasets[0].data = skyblock.y;
+        graph.data.datasets[1].data = economy.y;
 
-        eco.update()
-        net.update()
-
-
-
+        graph.update()
     }, 10000)
 })
